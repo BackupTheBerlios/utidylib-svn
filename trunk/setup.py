086a0ece81b17a57627342d78e43d056981e0ee0
corydodt <corydodt@98f5f959-65db-0310-a390-973c620f194d>
2004-02-20 17:10:39 +0000

    first checkin of utidylib, pre-0.2-ish

---
+++ trunk/setup.py
@@ -0,0 +1,61 @@
+from path import path
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.bdist_wininst import bdist_wininst
+from distutils import sysconfig
+import gendoc
+
+# pack the doc in as data files
+apidoc=path('apidoc')
+data_files = [] ; dfa = data_files.append
+if apidoc.isdir():
+    pkgdir = path('tidy')
+    dfa((str(pkgdir/apidoc), map(str, apidoc.files())))
+    for p in path('apidoc').walkdirs():
+        dfa((str(pkgdir/p), map(str, p.files())))
+
+
+class bdist_wininst_utidylib(bdist_wininst):
+    def finalize_options(self):
+        dfa = self.distribution.data_files.append
+        dfa((str(pkgdir), [str(pkgdir/'cygtidy-0-99-0.dll'),
+                           str(pkgdir/'README.tidydll')]
+             ))
+        private_ctypes = pkgdir/'pvt_ctypes'
+        dfa((str(private_ctypes), [str(private_ctypes/'ctypes.zip'),
+                                   str(private_ctypes/'_ctypes.pyd'),
+                                   str(private_ctypes/'README.ctypes')]
+             ))
+
+        # TODO - make it impossible to install on python2.2
+        bdist_wininst.finalize_options(self)
+
+# make sure data files are installed in tidylib package during binary 
+# build phase - this is evil.
+class install_data_utidylib(install_data):
+    def finalize_options (self):
+        self.set_undefined_options('install',
+                                   ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+    
+
+
+
+setup_data = dict(packages=['tidy', ],
+                  data_files=data_files,
+                  cmdclass=dict(install_data=install_data_utidylib,
+                                bdist_wininst=bdist_wininst_utidylib),
+                  name='uTidylib',
+                  version='0.2',
+                  author='Cory Dodt',
+                  author_email='corydodt@twistedmatrix.com',
+                  url='http://utidylib.sf.net',
+                  description='Wrapper for HTML Tidy -- '
+                              'http://tidy.sourceforge.net',
+                  long_description='''\
+A wrapper for the relocatable version of HTML Tidy (see
+http://tidy.sourceforge.net for details).  This allows you to
+tidy HTML files through a Pythonic interface.'''
+                  )
+
+setup(**setup_data)
---
+++ trunk/tidy/test_tidy.py
@@ -0,0 +1,64 @@
+﻿
+from twisted.trial import unittest
+import tidy
+import md5
+
+class TidyTestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        foo='''<html>
+    <h1>woot</h1>
+    <hr>
+    <img src=\'asdfasdf\'>
+    <p>é
+<!-- hhmts end -->
+  </body>
+</html>
+'''
+        file('foo.htm', 'w').write(foo)
+        self.input1="<html><script>1>2</script>"
+        self.input2="<html>\n" + "<p>asdkfjhasldkfjhsldjas\n" * 100
+    def defaultDocs(self):
+        doc1=tidy.parseString(self.input1)
+        doc2=tidy.parseString(self.input2)
+        doc3=tidy.parse("foo.htm")
+        doc4=tidy.parse("bar.htm") # doesn't exist
+        return (doc1, doc2, doc3, doc4)
+    def test_badOptions(self):
+        badopts=[{'foo': 1}, {'indent': '---'}, {'indent_spaces': None}]
+        for dct in badopts:
+            try:
+                tidy.parseString(self.input2, **dct)
+            except tidy.TidyLibError:
+                pass
+            else:
+                self.fail("Invalid option %s should have raised an error" %
+                          repr(dct))
+    def test_errors(self):
+        doc1, doc2, doc3, doc4=self.defaultDocs()
+        for doc in [doc1, doc2, doc3]:
+            str(getattr(doc, 'errors'))
+            self.assertEquals(doc1.errors[0].line, 1)
+    def test_options(self):
+        options = dict(wrap=30)
+        print tidy.parseString("<Html>asdlkhf alksfh alsdfh asldkfjh sdlfjh slhfasdf ", **options)
+        options = dict(add_xml_decl=1, show_errors=1, newline='CR', 
+                       output_xhtml=1)
+        doc2 = tidy.parseString("<Html>", **options)
+        self.failUnless(str(doc2).startswith('<?xml'))
+##        self.failIf(len(doc2.errors)>1) # FIXME - tidylib doesn't
+##                                        # support this?
+        self.failUnless(str(doc2).find('\n')<0)
+        doc3 = tidy.parse('foo.htm', output_encoding='utf8',
+                        alt_text='foo')
+        self.failUnless(str(doc3).find('\xc3\xa9')>=0)
+        self.failUnless(str(doc3).find('alt="foo"')>=0)
+    def test_parse(self):
+        doc1, doc2, doc3, doc4=self.defaultDocs()
+        digest = lambda name: md5.new(str(name)).hexdigest()
+        self.assertEqual(digest(doc1), '60534de02e2cf4f66b63ba601d1b74b7')
+        self.assertEqual(digest(doc2), 'f3b4f0c2fb5f14ed01b95db3d8289a4d')
+        self.assertEqual(digest(doc3), 'f9061475c3d0d08ca8c80fb48aa21655')
+    def test_unicode(self):
+        ''
+        # set options in unicode
+        # different output encodings
